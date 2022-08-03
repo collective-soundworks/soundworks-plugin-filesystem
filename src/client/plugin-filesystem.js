afc4185393c02ec1cdeab3ef5f769c290f31d40c
agolvet <alienor.golvet@gmail.com>
2022-08-03 14:53:30 +0200

    upload and delete

--- src/client/plugin-filesystem.js
+++ src/client/plugin-filesystem.js
@@ -29,6 +29,52 @@ const pluginFactory = function(AbstractPlugin) {
     get(name) {
       return this.state.get(name);
     }
+
+    upload(...args) {
+      let files, directory = null;
+      if (args.length === 1) {
+        files = args[0];
+      } else if (args.length === 2) {
+        directory = args[0];
+        files = args[1];
+      } else {
+        throw Error("@soundworks/plugin-filesystem's upload method only accepts 1 or 2 arguments");
+      }
+
+      const form = new FormData();
+      Object.entries(files).forEach(([filename, file]) => {
+        form.append(filename, file);
+      });
+
+      if (directory !== null) {
+        form.append('directory', directory);
+      }
+
+      fetch(`/s-${this.name}-upload`, {
+        method: 'POST',
+        body: form
+      });
+    }
+
+    delete(...args) {
+      let filename, directory = null;
+      if (args.length === 1) {
+        filename = args[0];
+      } else if (args.length === 2) {
+        directory = args[0];
+        filename = args[1];
+      } else {
+        throw Error("@soundworks/plugin-filesystem's delete method only accepts 1 or 2 arguments");
+      }
+
+      this.client.socket.send(`s:${this.name}:command`, {
+        action: 'delete',
+        payload: {
+          directory: directory,
+          filename: filename
+        },
+      });
+    }
   }
 }
 
